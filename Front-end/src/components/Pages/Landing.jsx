f1a7d5b06d372c1e297dce6cc62fb861147aafca
Cipactli Casillas <c.casillas19@gmail.com>
2025-06-17 14:02:07 +0200

    cambios landing y card pw

--- Front-end/src/components/Pages/Landing.jsx
+++ Front-end/src/components/Pages/Landing.jsx
@@ -1,9 +1,35 @@
 import Hero from "../Layout/Hero";
+import latestprojects from "../../../data/latestprojects.json";
+import { Link } from "react-router";
+import CardPreview from "../Projects/CardPreview";
+
 function Landing() {
   return (
     <>
-      {" "}
       <Hero />
+
+      <Link className="button--link" to="/create">
+        Crear un nuevo proyecto
+      </Link>
+
+      <ul>
+        {latestprojects.map((projectObj) => (
+          <li key={projectObj.id}>
+            <CardPreview
+              name={projectObj.name}
+              slogan={projectObj.slogan}
+              desc={projectObj.description}
+              technologies={projectObj.technologies}
+              demo={projectObj.demo}
+              repo={projectObj.repo}
+              author={projectObj.author}
+              job={projectObj.job}
+              photo={projectObj.photo}
+              image={projectObj.image}
+            />
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
