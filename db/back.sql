e5444f4073ea4007f0a3ffb154dff70bff1b73d0
Patri-GL <patrigl1986@gmail.com>
2025-06-24 11:39:01 +0100

    June 24 update: Connection to database

--- db/back.sql
+++ db/back.sql
@@ -6,46 +6,31 @@ VALUES ('Cristina Gómez', 'Frontend Developer', 'https://images.pexels.com/phot
 SELECT * FROM defaultdb.author;
 
 
-INSERT INTO defaultdb.project (name, slogan,repo,demo,technologies, `desc`) 
+INSERT INTO defaultdb.project (name, slogan,repo,demo,technologies, `desc`, photo) 
 VALUES ('GreenMarket',
 'El futuro del consumo responsable',
 'https://github.com/usuario/greenmarket',
 'https://greenmarket.demo.com','React, Node.js, MongoDB, Tailwind CSS',
-'Marketplace online enfocado en productos ecológicos, con integración de pagos y recomendaciones personalizadas.');
-SELECT * FROM defaultdb.project ;
-INSERT INTO defaultdb.project (name, slogan,repo,demo,technologies, `desc`) 
-VALUES ('TravelSync',
+'Marketplace online enfocado en productos ecológicos, con integración de pagos y recomendaciones personalizadas.',
+'https://www.canva.com/design/DAGqsYpCFOE/ED7VkGP92mYkHVIpK1CXBw/view?utm_content=DAGqsYpCFOE&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h8e1867cc57');
+('TravelSync',
 'Tu viaje, siempre sincronizado',
 'https://github.com/usuario/travelsync',
 'https://travelsync.demo.com',
 'Next.js, Supabase, Styled Components',
-'Aplicación colaborativa para planear viajes en grupo, compartir itinerarios y coordinar presupuestos.'), 
+'Aplicación colaborativa para planear viajes en grupo, compartir itinerarios y coordinar presupuestos.',
+'https://www.canva.com/design/DAGqsVC1kCQ/2O3E6kbLlPQDSLvjVXq6HA/view?utm_content=DAGqsVC1kCQ&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h20bbdc7360'), 
 ('Code4All',
 'Aprende a programar desde cero',
 'https://github.com/usuario/code4all',
 'https://code4all.demo.com',
 'Angular, Node.js, PostgreSQL',
-'Plataforma educativa con cursos interactivos de programación y foros colaborativos.'),
+'Plataforma educativa con cursos interactivos de programación y foros colaborativos.', 
+'https://www.canva.com/design/DAGqsTNFz8k/p0QDT3rWvuqrGMIWwmDebQ/view?utm_content=DAGqsTNFz8k&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h738b86fcc3'),
 ('WellnessApp',
 'Cuida tu mente y tu cuerpo',
 'https://github.com/usuario/wellnessapp',
 'https://wellnessapp.demo.com',
 'Vue.js, Firebase, Sass',
-'App móvil para el seguimiento de hábitos saludables, ejercicios de meditación y rutinas personalizadas.');
-SELECT * FROM defaultdb.project ;
-INSERT INTO defaultdb.project (photo)
-VALUES ('https://www.canva.com/design/DAGqsYpCFOE/ED7VkGP92mYkHVIpK1CXBw/view?utm_content=DAGqsYpCFOE&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h8e1867cc57'),
-('https://www.canva.com/design/DAGqsVC1kCQ/2O3E6kbLlPQDSLvjVXq6HA/view?utm_content=DAGqsVC1kCQ&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h20bbdc7360'),
-('https://www.canva.com/design/DAGqsTNFz8k/p0QDT3rWvuqrGMIWwmDebQ/view?utm_content=DAGqsTNFz8k&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h738b86fcc3'),
-('https://www.canva.com/design/DAGqsdkuEQA/2Q18Mc2M61whEeRgszkzOQ/view?utm_content=DAGqsdkuEQA&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h0e8b59224f');
-UPDATE defaultdb.project SET photo = 'https://www.canva.com/design/DAGqsYpCFOE/ED7VkGP92mYkHVIpK1CXBw/view?utm_content=DAGqsYpCFOE&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h8e1867cc57' WHERE idproject = 1;
-UPDATE defaultdb.project 
-SET photo = 'https://www.canva.com/design/DAGqsVC1kCQ/2O3E6kbLlPQDSLvjVXq6HA/view?utm_content=DAGqsVC1kCQ&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h20bbdc7360' 
-WHERE idproject = 2;
-UPDATE defaultdb.project 
-SET photo = 'https://www.canva.com/design/DAGqsTNFz8k/p0QDT3rWvuqrGMIWwmDebQ/view?utm_content=DAGqsTNFz8k&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h738b86fcc3' 
-WHERE idproject = 3;
-UPDATE defaultdb.project 
-SET photo = 'https://www.canva.com/design/DAGqsdkuEQA/2Q18Mc2M61whEeRgszkzOQ/view?utm_content=DAGqsdkuEQA&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h0e8b59224f'
-WHERE idproject = 4;
-SELECT * FROM defaultdb.project ;
+'App móvil para el seguimiento de hábitos saludables, ejercicios de meditación y rutinas personalizadas.',
+'https://www.canva.com/design/DAGqsdkuEQA/2Q18Mc2M61whEeRgszkzOQ/view?utm_content=DAGqsdkuEQA&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h0e8b59224f');
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const data = require("../Front-end/data/latestprojects.json");
+// const data = require("../Front-end/data/latestprojects.json");
 // Creamos una vari con el servidor
 const server = express();
-
+const mysql = require("mysql2/promise");
 // Configuramos server para que funcione bien como API
 server.use(cors());
 server.use(express.json());
@@ -25,19 +25,49 @@ server.listen(port, () => {
 
 // ENDPOINTS DE APIS
 
-// server.get("/api/proyectos", (req, res) => {
+// server.get("/api/proyectos", async (req, res) => {
+// 1. Conn a la base de datos
+
+async function getConnection() {
+  const datosConexion = {
+    host: "adalabpromo53team-01-proyectoteam1-03bb.i.aivencloud.com",
+    port: "16388",
+    user: "avnadmin",
+    password: "AVNS_bzNzUlFkQJay8B1BovI",
+    database: "defaultdb",
+  };
+  const conexion = await mysql.createConnection(datosConexion);
+  await conexion.connect();
+  return conexion;
+}
+server.get("/api/project", async (req, res) => {
+  console.log(req.query);
+
+  const conn = await getConnection();
+  // 2. Lanzamos el SELECT a la base de datos
+  const query = `SELECT 
+  *
+  FROM defaultdb.author
+  JOIN defaultdb.project ON author.idauthor = project.author_idauthor;`;
+  const [results] = await conn.query(query);
+  // 3. Desconectamos/Terminamos la conn
+  await conn.end();
+  // 4. Devolvemos el JSON con lo sdatos en la res
+  res.json(results);
+});
+
 //   res.json({
 //     success: true,
-//     proyectos: [{}, {}],
+//     proyectos: datosConexion,
 //   });
 // });
 
 // SERVIDOR DE FICHEROS ESTÁTICOS
 
-server.use(express.static(path.join(__dirname, "../Front-end")));
+// NO BORRAR ! server.use(express.static(path.join(__dirname, "../Front-end/dist")));
 
 // NO ENCONTRADO
 
-server.use(/.*/, (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "../FRONT-END/error404.html"));
-});
+// server.use(/.*/, (req, res) => {
+//   res.status(404).sendFile(path.join(__dirname, "../FRONT-END/error404.html"));
+// });
